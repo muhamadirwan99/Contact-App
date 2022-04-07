eccd54a81c5428d9f9de628eac6f39def8f602be
Muhamad Irwan <muhamadirwan99@gmail.com>
2022-04-07 14:52:05 +0700

    Finish contact app

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { argv } = require("process");
 const yargs = require("yargs");
 const contacts = require('./contacts');
 
@@ -24,6 +25,47 @@ yargs.command({
     handler(argv) {
         contacts.simpanContact(argv.nama, argv.email, argv.noHP);
     },
+}).demandCommand();
+
+// menampilkan daftar semua nama & no hp contact
+yargs.command({
+    command: 'list',
+    describe: 'Menampilkan semua nama & no HP contact',
+    handler() {
+        contacts.listContact();
+    }
+});
+
+//menampilkan detail sebuah contact
+yargs.command({
+    command: 'detail',
+    describe: 'Menampilkan detail sebuah contact berdasarkan nama',
+    builder: {
+        nama: {
+            describe: 'Nama lengkap',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+    handler(argv) {
+        contacts.detailContact(argv.nama);
+    }
+});
+
+//menghapus kontak berdasarkan nama
+yargs.command({
+    command: 'delete',
+    describe: 'Menghapus sebuah contact berdasarkan nama',
+    builder: {
+        nama: {
+            describe: 'Nama lengkap',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+    handler(argv) {
+        contacts.deleteContact(argv.nama);
+    }
 });
 
 yargs.parse();
